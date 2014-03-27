c97a01825465668e2ff7ceff8f11fdb2a68f395a
bsmitty <b671983@gmail.com>
2014-03-26 20:21:52 -0400

    ramdisk updates --- osm0sis

--- ramdisk-DirtyV/sbin/dvbootscript.sh
+++ ramdisk-DirtyV/sbin/dvbootscript.sh
@@ -12,36 +12,32 @@ $bb [ -e /system/etc/sysctl.conf ] && $bb mv -f /system/etc/sysctl.conf /system/
 $bb [ -e /system/lib/hw/power.tuna.so.dvbak ] || $bb cp /system/lib/hw/power.tuna.so /system/lib/hw/power.tuna.so.dvbak;
 $bb [ -e /system/lib/hw/power.tuna.so ] && $bb rm -f /system/lib/hw/power.tuna.so;
 
+# backup and replace Host AP Daemon for working Wi-Fi tether on 3.4 kernel Wi-Fi drivers
+$bb [ -e /system/bin/hostapd.dvbak ] || $bb cp /system/bin/hostapd /system/bin/hostapd.dvbak;
+$bb cp -f /sbin/hostapd /system/bin/;
+chown root.shell /system/bin/hostapd;
+chmod 755 /system/bin/hostapd;
+
 # backup and replace Media Codec Profiles if on SR builds, restore if not, and push init.d script for other kernels
 case `uname -r` in
   *DirtyV-SR)
     $bb [ -e /system/etc/media_profiles.xml.dvbak ] || $bb cp /system/etc/media_profiles.xml /system/etc/media_profiles.xml.dvbak;
     $bb cp -f /sbin/media_profiles.xml /system/etc/;
-    $bb chmod 644 /system/etc/media_profiles.xml;;
+    chmod 644 /system/etc/media_profiles.xml;;
   *)
     $bb [ -e /system/etc/media_profiles.xml.dvbak ] && $bb mv -f /system/etc/media_profiles.xml.dvbak /system/etc/media_profiles.xml;
-    $bb chmod 644 /system/etc/media_profiles.xml;;
+    chmod 644 /system/etc/media_profiles.xml;;
 esac;
 $bb cp -f /sbin/dvmediarevert /system/etc/init.d/;
-$bb chmod 755 /system/etc/init.d/dvmediarevert;
-
-
-$bb cp -f /sbin/hostapd /system/bin/hostapd;
-$bb chmod 755 /system/bin/hostapd;
-$bb chown root.shell system/bin/hostapd;
-
-
+chmod 755 /system/etc/init.d/dvmediarevert;
 
 # create and set permissions for /system/etc/init.d if it doesn't already exist
 if [ ! -e /system/etc/init.d ]; then
-  $bb mkdir /system/etc/init.d;
-  $bb chown -R root.root /system/etc/init.d;
-  $bb chmod -R 755 /system/etc/init.d;
+  mkdir /system/etc/init.d;
+  chown -R root.root /system/etc/init.d;
+  chmod -R 755 /system/etc/init.d;
 fi;
 $bb mount -o ro,remount /system;
-
-# set up Synapse support
-/sbin/uci;
 
 # fix permissions for any included governors (and older underlying ramdisks)
 governor=reset;
@@ -50,19 +46,19 @@ while sleep 1; do
   if [ $governor != $current ]; then
     governor=$current;
     for i in /sys/devices/system/cpu/cpufreq/*; do
-      $bb chown system:system $i/*;
-      $bb chmod 664 $i/*;
+      chown system:system $i/*;
+      chmod 664 $i/*;
     done;
   fi;
 done&
 
 # disable debugging
-echo "0" > /sys/module/wakelock/parameters/debug_mask;
-echo "0" > /sys/module/userwakelock/parameters/debug_mask;
-echo "0" > /sys/module/earlysuspend/parameters/debug_mask;
-echo "0" > /sys/module/alarm/parameters/debug_mask;
-echo "0" > /sys/module/alarm_dev/parameters/debug_mask;
-echo "0" > /sys/module/binder/parameters/debug_mask;
+echo 0 > /sys/module/wakelock/parameters/debug_mask;
+echo 0 > /sys/module/userwakelock/parameters/debug_mask;
+echo 0 > /sys/module/earlysuspend/parameters/debug_mask;
+echo 0 > /sys/module/alarm/parameters/debug_mask;
+echo 0 > /sys/module/alarm_dev/parameters/debug_mask;
+echo 0 > /sys/module/binder/parameters/debug_mask;
 
 # suitable configuration to help reduce network latency
 echo 2 > /proc/sys/net/ipv4/tcp_ecn;
@@ -76,6 +72,18 @@ for i in /sys/class/net/*; do
   echo 0 > $i/tx_queue_len;
 done;
 
+# increase sched timings
+echo 15000000 > /proc/sys/kernel/sched_latency_ns;
+echo 2000000 > /proc/sys/kernel/sched_min_granularity_ns;
+echo 3000000 > /proc/sys/kernel/sched_wakeup_granularity_ns;
+
+# adjust background app cgroup priority
+echo 91 > /dev/cpuctl/apps/bg_non_interactive/cpu.shares;
+echo 400000 > /dev/cpuctl/apps/bg_non_interactive/cpu.rt_runtime_us;
+
+# initialize timer slack
+echo 100000000 > /dev/cpuctl/apps/bg_non_interactive/timer_slack.min_slack_ns;
+
 # decrease fs lease time
 echo 10 > /proc/sys/fs/lease-break-time;
 
@@ -83,10 +91,7 @@ echo 10 > /proc/sys/fs/lease-break-time;
 echo 128 > /proc/sys/kernel/random/read_wakeup_threshold;
 echo 256 > /proc/sys/kernel/random/write_wakeup_threshold;
 
-# initialize timer slack
-echo 100000000 > /dev/cpuctl/apps/bg_non_interactive/timer_slack.min_slack_ns;
-
-# disable ASLR
+# disabled ASLR to increase AEM-JIT cache hit rate
 echo 0 > /proc/sys/kernel/randomize_va_space;
 
 # double the default minfree kb
@@ -123,25 +128,31 @@ while sleep 1; do
   fi;
 done&
 
-# wait for systemui and increase its priority
+# set up Synapse support
+/sbin/uci;
+
+# wait for systemui and increase some process priorities
 while sleep 1; do
   if [ `$bb pidof com.android.systemui` ]; then
     systemui=`$bb pidof com.android.systemui`;
+    echo $systemui > /dev/cpuctl/tasks;
+    echo -17 > /proc/$systemui/oom_adj;
     $bb renice -18 $systemui;
-    $bb echo -17 > /proc/$systemui/oom_adj;
-    $bb chmod 100 /proc/$systemui/oom_adj;
+    $bb renice 5 `$bb pgrep kswapd`;
     exit;
   fi;
 done&
 
-# lmk whitelist for common launchers and increase launcher priority
-list="com.android.launcher com.google.android.googlequicksearchbox org.adw.launcher org.adwfreak.launcher net.alamoapps.launcher com.anddoes.launcher com.android.lmt com.chrislacy.actionlauncher.pro com.cyanogenmod.trebuchet com.gau.go.launcherex com.gtp.nextlauncher com.miui.mihome2 com.mobint.hololauncher com.mobint.hololauncher.hd com.qihoo360.launcher com.teslacoilsw.launcher com.tsf.shell org.zeam";
+# lmk whitelist for common launchers+systemui and increase launcher priority
+list="com.android.launcher com.google.android.googlequicksearchbox org.adw.launcher org.adwfreak.launcher net.alamoapps.launcher com.anddoes.launcher com.android.lmt com.chrislacy.actionlauncher.pro com.cyanogenmod.trebuchet com.gau.go.launcherex com.gtp.nextlauncher com.miui.mihome2 com.mobint.hololauncher com.mobint.hololauncher.hd com.mycolorscreen.themer com.qihoo360.launcher com.teslacoilsw.launcher com.tsf.shell org.zeam";
+echo 1 > /sys/module/lowmemorykiller/parameters/donotkill_sysproc;
 while sleep 60; do
   for class in $list; do
     if [ `$bb pgrep $class | head -n 1` ]; then
       launcher=`$bb pgrep $class`;
-      $bb echo -17 > /proc/$launcher/oom_adj;
-      $bb chmod 100 /proc/$launcher/oom_adj;
+      echo "com.android.systemui,$class" > /sys/module/lowmemorykiller/parameters/donotkill_sysproc_names;
+      [ -e /sdcard/Synapse/lmk_whitelists/.sys_processes -a -z `cat /sdcard/Synapse/lmk_whitelists/.sys_processes` ] && echo "com.android.systemui\n$class" > /sdcard/Synapse/lmk_whitelists/.sys_processes;
+      echo -17 > /proc/$launcher/oom_adj;
       $bb renice -18 $launcher;
     fi;
   done;
