c927dcc7eef5580e7b726ad0c8f7b3e6bf34aa06
bsmitty <b671983@gmail.com>
2014-04-22 19:41:29 -0400

    OMAP: process tput request made before PM init.
    Because driver load order is uncertain, drivers could request bus
    throughput constraints before the PM subsystem had initialized.  The
    constraints would be recorded, but would have no effect on the actual
    L3 clock.  This change suppresses attempts to set L3 scaling before PM
    has initialized, and causes the PM subsystem to apply any pending bus
    throughput constraints immediately after it has initialized itself.
    
    Change-Id: If2a1f5315b8c1b084e4e4b3ce8e6844d92dd59b6
    Signed-off-by: John Grossman <johngro@google.com>
    Signed-off-by: Ruslan Bilovol <ruslan.bilovol@ti.com>

--- arch/arm/mach-omap2/pm44xx.c
+++ arch/arm/mach-omap2/pm44xx.c
@@ -1581,6 +1581,9 @@ static int __init omap4_pm_init(void)
 	/* let the other CPU know as well */
 	smp_wmb();
 
+	/* apply any pending bus throughput requests */
+	omap_pm_apply_min_bus_tput();
+
 err2:
 	return ret;
 }
